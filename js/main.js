4155032cb102c3a16132bfad6db51d76491899a8
kirill <chkrvl@gmail.com>
2019-06-22 23:09:53 +0400

    Добавил перемещение маркера .map__pin--main

--- js/main.js
+++ js/main.js
@@ -123,7 +123,44 @@ var fillAdressInput = function () {
   adressFormInput.value = address;
 };
 
-mainMapPin.addEventListener('click', onMainMapPinClick);
+// *****
+
+mainMapPin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY,
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mainMapPin.style.top = (mainMapPin.offsetTop - shift.y) + 'px';
+    mainMapPin.style.left = (mainMapPin.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+  mainMapPin.addEventListener('click', onMainMapPinClick);
+});
 
 var priceFormInput = adForm.querySelector('#price');
 var typeFormSelect = adForm.querySelector('#type');
