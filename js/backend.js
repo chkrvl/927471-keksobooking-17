b6c7924d445d6a4e9fd76b05024f368a1a320020
kirill <chkrvl@gmail.com>
2019-07-05 09:49:54 +0400

    Заменил магические числа константами

--- js/backend.js
+++ js/backend.js
@@ -1,12 +1,15 @@
 'use strict';
 
 (function () {
+  var SUCCESS_ANSWER_CODE = 200;
+  var MAX_TIMEOUT = 10000; //в миллисекундах
+
   window.load = function (url, onSuccess, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
     xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
+      if (xhr.status === SUCCESS_ANSWER_CODE) {
         onSuccess(xhr.response);
       } else {
         onError();
@@ -21,7 +24,7 @@
       onError();
     });
 
-    xhr.timeout = 10000; // 10s
+    xhr.timeout = MAX_TIMEOUT;
 
     xhr.open('GET', url);
     xhr.send();
